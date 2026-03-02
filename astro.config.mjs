e7e05f9342dde5f8bf96aabba2ed6385c5574d50
sarada0909 <sarada0909@gmail.com>
2026-03-02 14:25:21 +0900

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://my-ai-blog-9pe.pages.dev/',
   vite: {
     plugins: [tailwindcss()]
   },
-
+  integrations: [sitemap()],
   adapter: cloudflare()
 });
